7d015d06d4bbc7a1c0d896200a3ee12a80de6fc5
Lucas Zacharewicz <lucas.zacharewicz@gmail.com>
2020-01-19 11:15:59 -0500

    added new training model

--- Model/train.py
+++ Model/train.py
@@ -1,9 +1,12 @@
 from imageai.Detection.Custom import DetectionModelTrainer
 
+'''
+["Rick Owens Ramones", "Rick Owens Geobaskets", "Rick Owens x Adidas Runners", "Rick Owens x Adidas Tech Runners", "Rick Owens x Adidas Level Runners", "Balenciaga Arenas", "Balenciaga Speed Sneaker", "Balenciaga Tripple S", "Common Project Achillies Low", "Common Project Chelsea Boot", "Dr Martens 1460"]
+'''
+
 trainer = DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
-trainer.setDataDirectory(data_directory="rick owens ramones")
-trainer.setTrainConfig(object_names_array=["Rick Owens Ramones"], batch_size=4, num_experiments=50, train_from_pretrained_model="pretrained-yolov3.h5")
-# In the above,when training for detecting multiple objects,
-#set object_names_array=["object1", "object2", "object3",..."objectz"]
+trainer.setDataDirectory(data_directory="shoetect")
+trainer.setTrainConfig(object_names_array=["Rick Owens Ramones", "Rick Owens Geobaskets", "Rick Owens x Adidas Runners", "Rick Owens x Adidas Tech Runners", "Rick Owens x Adidas Level Runners", "Balenciaga Arenas", "Balenciaga Speed Sneaker", "Balenciaga Tripple S", "Common Project Achillies Low", "Common Project Chelsea Boot", "Dr Martens 1460"], batch_size=4, num_experiments=50, train_from_pretrained_model="pretrained-yolov3.h5")
+
 trainer.trainModel()
